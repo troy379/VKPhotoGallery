a87c9fc55eacc4ae168ce1a0fc9618032e446e15
troy379 <troy.carvill@gmail.com>
2016-12-16 19:39:44 +0200

    realm init

--- app/src/main/java/com/stfalcon/vkphotogallery/App.java
+++ app/src/main/java/com/stfalcon/vkphotogallery/App.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
+import io.realm.Realm;
+
 /*
  * Created by troy379 on 16.12.16.
  */
@@ -14,5 +16,6 @@ public class App extends Application {
         super.onCreate();
 
         Fresco.initialize(this);
+        Realm.init(this);
     }
 }
